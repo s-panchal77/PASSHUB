7adfc9336ff1888955312f5315cd7d9aa1b962ad
s-panchal77 <sarthak7704@gmail.com>
2025-08-30 19:14:01 +0530

    add GitHub icon in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,17 @@ const Navbar = () => {
 
       <div className="mycontainer flex justify-between items-center px-4 h-16">
         <div className="logo font-bold text-2xl">PASS-HUB</div>
-        <ul>
+        {/* <ul>
             <li className='flex gap-4'>
                 <a className='hover:font-bold' href="/">Home</a>
                 <a className='hover:font-bold' href="#">About</a>
                 <a className='hover:font-bold' href="#">Contact</a>
             </li>
-        </ul>
+        </ul> */}
+        <button className='bg-gray-300 hover:bg-gray-400 cursor-pointer rounded'>
+          <img src="/icons/github.svg" alt="github logo" className='inline-block w-10 p-1' />
+          <span className='text-gray-900 font-bold px-2'>GitHub</span>
+        </button>
       </div>
     </nav>
   )
